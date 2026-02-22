09351deec7f75d7f0dce7816d2f9e1b1f30e0e9f
yiugsool <lovesjy1314520@gmail.com>
2026-02-22 13:37:35 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'dsa-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// 安装时缓存所有文件
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 断网时直接从手机本地提取缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
